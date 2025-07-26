4f0259fbadaeb3af3446d7fddec764d336d233e5
Tóth Norbert <tnorbert201@gmail.com>
2025-07-26 23:07:27 +0200

    Update

--- lua/scallyt/init.lua
+++ lua/scallyt/init.lua
@@ -2,10 +2,10 @@ require("scallyt.set")
 require("scallyt.remap")
 require("scallyt.lazy_init")
 require("mason").setup()
-vim.cmd [[colorscheme moonfly]]
+vim.cmd [[colorscheme kanagawa]]
 
 function ColorMyPencil()
-    color = color or "moonfly"
+    color = color or "kanagawa"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
